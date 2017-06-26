5747a3ebe58e5f9e7676fac55a30ba68d7d47d69
marcwittke <marc@wittke-web.de>
2017-06-26 12:07:04 -0300

    reproduction of loader exception

---
+++ WebApplication8/NetStandardLib/MyRegistry.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NetStandardLib
+{
+    using FluentScheduler;
+
+    public class HelloJob : IJob
+    {
+        public void Execute()
+        {
+            Console.WriteLine("Hello");
+        }
+    }
+
+    public class MyRegistry : Registry
+    {
+        public MyRegistry()
+        {
+            Schedule<HelloJob>().ToRunNow().AndEvery(10).Seconds();
+        }
+    }
+}
